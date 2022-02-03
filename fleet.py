7765cc5ce6834243eb881b06657285a7745e26ca
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-03 15:32:20 -0600

    Added instance variables and methods for the Fleet class.

--- fleet.py
+++ fleet.py
@@ -4,7 +4,16 @@ from robot import Robot
 class Fleet:
     
     def __init__(self) -> None:
-        pass
+        
+        self.fleet = []
+        self.create_fleet()
 
     def create_fleet(self): #void
-        pass
+
+        robo_1 = Robot('Terminator', 15)
+        robo_2 = Robot('Robo Cop', 20)
+        robo_3 = Robot('Gypsy Danger', 25)
+
+        self.fleet.append(robo_1)
+        self.fleet.append(robo_2)
+        self.fleet.append(robo_3)
